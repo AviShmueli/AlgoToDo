c632e58286f7c17acb811eaa13130d466d37a0f0
Avi Shmueli <avis@algo.bz>
2016-10-05 14:27:42 +0300

    first test

--- AlgoToDo/server/app.js
+++ AlgoToDo/server/app.js
@@ -24,12 +24,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use(express.static('./www'));
+app.use(express.static('./bower_components'));
+app.use(express.static('./node_modules'));
 app.use(express.static('../www'));
 app.use(express.static('../bower_components'));
 app.use(express.static('../node_modules'));
 
 server.listen(port, function (err) {
-    console.log('running server on port ' + port);
+    console.log('avi: running server on port ' + port);
 });
 
 // -------- Socket.io --------//
