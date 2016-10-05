e77dde986841f72e63b32ba3cbac36dcb12eca85
Avi Shmueli <avis@algo.bz>
2016-10-05 15:48:16 +0300

    app

--- AlgoToDo/server/app.js
+++ AlgoToDo/server/app.js
@@ -24,6 +24,12 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use(express.static('AlgoToDo/www'));
+app.use(express.static('AlgoToDo/bower_components'));
+app.use(express.static('AlgoToDo/node_modules'));
+app.use(express.static('www'));
+app.use(express.static('bower_components'));
+app.use(express.static('node_modules'));
 app.use(express.static('/www'));
 app.use(express.static('/bower_components'));
 app.use(express.static('/node_modules'));
