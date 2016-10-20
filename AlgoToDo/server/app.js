cf7a2cd760ec106dc17f60089ec5e023254c97fe
Avi Shmueli <avis@algo.bz>
2016-10-20 13:28:13 +0300

    fix

--- AlgoToDo/server/app.js
+++ AlgoToDo/server/app.js
@@ -232,8 +232,8 @@ app.post('/TaskManeger/sendRegistrationId', function (req, res) {
 
 app.get('/TaskManeger/getTasks', function (req, res) {
     
-    //var me = req.data.user;
-    console.log("user asks for all tasks\n", req.query);
+    var me = req.query.user;
+    //console.log("user asks for all tasks\n", req.query);
     mongodb.connect(mongoUrl, function (err, db) {
         console.log(err);
         var collection = db.collection('tasks');
