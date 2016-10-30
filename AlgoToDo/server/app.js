4a9eac55ca073759f8e5855aa7a981b17cdaa803
Avi Shmueli <avis@algo.bz>
2016-10-30 11:44:41 +0200

    fix

--- AlgoToDo/server/app.js
+++ AlgoToDo/server/app.js
@@ -63,7 +63,7 @@ var pushTaskToAndroidUser = function (task) {
             sound: 'default',
             icon: 'res://icons/android/icon-48-mdpi.png',
             body: task.description,
-            badge: "✡"
+            badge: "1"
         }
         
     });
