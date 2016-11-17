376f5986bb5c7bc72aa39a96b88ce3ebabb64690
avi <avis@algo.bz>
2016-11-17 13:56:55 +0200

    fix

--- AlgoToDo/server/app.js
+++ AlgoToDo/server/app.js
@@ -101,7 +101,7 @@ console.log("apnProvider: ", apnProvider);
 
 var pushTaskToAppleUser = function(task){
     
-    var deviceToken = 'f16a3c6261a8d3512c2a968a3f1430d8a76baa598c92625f10d21f749baddba4';//task.ApnRegistrationId;
+    var deviceToken = task.to.ApnRegistrationId;
     var deviceTokenInHex = Buffer.from(deviceToken, 'base64').toString('hex');
     
     var note = new apn.Notification();
@@ -110,7 +110,7 @@ var pushTaskToAppleUser = function(task){
     note.badge = 1;
     note.priority = 10;
     note.sound = "ping.aiff";
-    note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+    note.alert = "משימה חדשה מ" + task.from.name;//"\uD83D\uDCE7 \u2709 You have a new message";
     note.payload = {'messageFrom': 'John Appleseed'};
     note.topic = "com.algotodo.app";
     
@@ -134,7 +134,7 @@ var pushTaskToAppleUser = function(task){
 
 }
 
-pushTaskToAppleUser({});
+//pushTaskToAppleUser({});
 
 /* ----- GCM ------ */
 var gcm = require('node-gcm');
