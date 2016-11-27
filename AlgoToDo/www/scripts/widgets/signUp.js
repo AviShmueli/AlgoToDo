a448fcd331642d7f9e1ee01981c9a843e7278214
Avi Shmueli <avis@algo.bz>
2016-11-27 21:13:31 +0200

    fix

--- AlgoToDo/www/scripts/widgets/signUp.js
+++ AlgoToDo/www/scripts/widgets/signUp.js
@@ -23,6 +23,13 @@
             vm.inProgress = false;
             vm.user = $scope.user;
             vm.signUp = $scope.signUp;
+
+            vm.signMeUp = function () {
+                if (vm.inProgress === false) {
+                    vm.inProgress = true;
+                    vm.signUp();
+                }
+            }
         }
 
         return directive;
