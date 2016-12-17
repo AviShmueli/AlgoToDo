4f150771b86344eb334d420b26b46881108dbac4
Avi Shmueli <avis@algo.bz>
2016-12-17 20:47:07 +0200

    fix

--- AlgoToDo/www/scripts/cordova/cordovaPlugins.js
+++ AlgoToDo/www/scripts/cordova/cordovaPlugins.js
@@ -346,11 +346,13 @@
 
         var takePicture = function (sourceType) {
             var _sourceType = sourceType === 'camera' ? Camera.PictureSourceType.CAMERA : Camera.PictureSourceType.PHOTOLIBRARY;
+            var isSamsungDevice = datacontext.getDeviceDetailes() !== undefined && datacontext.getDeviceDetailes().manufacturer === "samsung";
+
             var options = {
                 quality: 100,
                 destinationType: Camera.DestinationType.FILE_URI,
                 sourceType: _sourceType,
-                allowEdit: true,
+                allowEdit: isSamsungDevice,
                 encodingType: Camera.EncodingType.JPEG,
                 targetWidth: window.innerWidth,
                 targetHeight: window.innerHeight,
