d072ee4deba765e2cf0814a5a7c1a1c1828b3454
avi <avis@algo.bz>
2017-01-26 20:40:01 +0200

    fix

--- AlgoToDo/typings/cordova-typings.d.ts
+++ AlgoToDo/typings/cordova-typings.d.ts
@@ -8,3 +8,5 @@
 /// <reference path="../.vscode/typings/cordova/plugins/FileSystem.d.ts"/>
 /// <reference path="../.vscode/typings/cordova/plugins/FileTransfer.d.ts"/>
 /// <reference path="../.vscode/typings/cordova/plugins/StatusBar.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Dialogs.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Globalization.d.ts"/>
