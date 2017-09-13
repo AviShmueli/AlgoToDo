46d835bc49f873f5905c0134a47a7cee1ba4b96d
Avi Shmueli <avis@algo.bz>
2017-09-13 22:15:02 +0300

    test

--- AlgoToDo/www/scripts/app.constans.js
+++ AlgoToDo/www/scripts/app.constans.js
@@ -2,5 +2,6 @@
     'use strict';
 
     angular.module('app')
-    .constant('MAIN_CLIQA_ID', '585c1e28ee630b29fc4b2d3d');
+    .constant('MAIN_CLIQA_ID', '585c1e28ee630b29fc4b2d3d')
+    .constant('MAIN_CLIQA_ID2', '222');
 })();
